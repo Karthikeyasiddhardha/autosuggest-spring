ffb6fe238dc74b739028037bc9368eb8535b0c27
Chandra Karthikeya <ckarthikeya2007@gmail.com>
2026-07-18 12:19:12 +0530

    toggle

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,37 @@
-//alert("slow wifi");
-console,lol("hello from js");
+var id
+// alert("Hello guysss");
+
+console.log("hello from JS!")
+
+var users = [
+    {
+        "name" : "john doe",
+        "gender" : "male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "jane doe",
+        "gender" : "female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
